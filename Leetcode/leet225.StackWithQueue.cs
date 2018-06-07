332c1d65ba9cd7f96a154bf1e1765bb83c539bb5
wildWind <binpan0423@gmail.com>
2018-06-07 15:46:56 +0800

    leet 232 & 225  Stack Queue

---
+++ Leetcode/leet225.StackWithQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MyStack
+{
+
+    private Queue<int> queue;
+    /** Initialize your data structure here. */
+    public MyStack()
+    {
+        queue = new Queue<int>();
+    }
+
+    /** Push element x onto stack. */
+    public void Push(int x)
+    {
+        queue.Enqueue(x);
+        for (int i = 0; i != queue.Count -1 ; ++i)
+        {
+            queue.Enqueue(queue.Peek());
+            queue.Dequeue();
+        }
+
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int Pop()
+    {
+        return queue.Dequeue();
+    }
+
+    /** Get the top element. */
+    public int Top()
+    {
+        return queue.Peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public bool Empty()
+    {
+        return queue.Count == 0;
+    }
+}
---
+++ Leetcode/leet232.QueueWithStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class MyQueue
+{
+    private Stack<int> stack1, stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue()
+    {
+        stack1 = new Stack<int>();
+        stack2 = new Stack<int>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void Push(int x)
+    {
+        //永远是2入栈，1出栈
+        stack2.Push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int Pop()
+    {
+        if (stack1.Count == 0)
+        {
+            while (stack2.Count != 0)
+            {
+                stack1.Push(stack2.Pop());
+            }
+        }
+        return stack1.Pop();
+    }
+
+    /** Get the front element. */
+    public int Peek()
+    {
+        if (stack1.Count == 0)
+        {
+            while (stack2.Count != 0)
+            {
+                stack1.Push(stack2.Pop());
+            }
+        }
+        return stack1.Peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public bool Empty()
+    {
+        return stack1.Count == 0 && stack2.Count == 0;
+    }
+}
+
